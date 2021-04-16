52ce3bdbc4faf57b57b2bdca7b9a41c735b45f53
mins <heroms01@gmail.com>
2021-04-16 03:19:41 +0000

    add template

---
+++ template.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestCase(unittest.TestCase):
+	def test1(self):
+		self.assertEqual(solution(1), 4)
+
+		
+def solution(board):
+	answer = 0
+	return answer
+	
+unittest.main()
